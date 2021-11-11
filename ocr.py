d1075946da31c282e06664384ea73845c45e489b
Ozan ERCAN <ozanercann@gmail.com>
2021-11-11 16:24:19 +0300

    Update ocr.py
    Description added

--- ocr.py
+++ ocr.py
@@ -1,6 +1,8 @@
-# bu programin yanina resimler diye bir klasor acip içine OCR dan gecirmek istediginiz resimleri kopyalayin
-# python3 ocr.py   komutuyla resimlerdeki yazi ve karakterleri txt dosyasina bastirin.
+# Tesseract-based OCR program developed to convert text and characters in images to text  
 
+# USAGE #
+# Open a folder called "images" in the directory where this program is located and copy the pictures you want to OCR into.
+# With the python3 ocr.py command, print the text and characters in the images to the txt file.
 
 
 from PIL import Image
@@ -9,19 +11,15 @@ import pyocr.builders
 import os
 
 
-# tum resimlerin dosya yollarini input.txt dosyasina yazdirir.
-os.system('ls resimler >> input.txt')
+os.system('ls images >> input.txt')
 
-# alinan dosya yollarini kullanmak için arraye kopyalanir.
 inputarr = []
 with open('input.txt') as my_file:
     for line in my_file:
         inputarr.append(line)
 
-
-
-#  tesseract OCR kodu çalisan kisim.
 tools = pyocr.get_available_tools()
+
 if len(tools) == 0:
     print("No OCR tool found")
     sys.exit(1)
@@ -36,7 +34,7 @@ print("Will use lang '%s'" % lang)
 
 outCount=0
 
-# OCR dan elde edilen outputlari txt dosyalarina yazdirir.
+
 for x in inputarr:
     x = x[:-1]
     txt = tool.image_to_string(
